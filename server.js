8a20eca2ed445e5bb6df03ac36a3f11ba1c7a23e
shashidhar <shashidhar>
2023-10-22 00:59:06 -0400

    Added item route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -9,6 +10,15 @@ const port =  process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB connection successful");
+})
+
+app.use('/item', require('./item'))
 
 app.listen(port, () => {
     console.log(`Server is running on the port : ${port}`);
